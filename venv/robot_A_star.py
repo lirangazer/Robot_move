9d7b8a9d435d45fa2a2fdfaa12a0452f37b5e4e0
lirangazer <lirangazer@gmail.com>
2021-04-29 09:03:31 +0300

    adding:  1.check grid function  2.get block value in for loop

--- venv/robot_A_star.py
+++ venv/robot_A_star.py
@@ -1,12 +1,25 @@
-
+def check_grid(grid, board_size):
+    numOfBolck = 0
+    for i in grid:
+       numOfBolck+=len(i)
+    if(numOfBolck != (board_size * board_size)):
+        print("there are missing info to solve the problem please fix and try again ")
+    return
 
 def main():
     input_file = open('files\input.txt','r')
     lines = input_file.read().splitlines()
     algorithem_type=lines[0]
-    borad_size = lines[1]
-    grid = [list(lines[2]),list(lines[3]) ,list(lines[4])]
-    
+    borad_size = int(lines[1])
+    grid=[]
+    currentBoard = 0
+    count = 0
+    for line in lines[2:]:
+        grid.append(list(line))
+
+    movement =['U','D','R','L']
+    cost={'road': 1 , 'dirt_road' : 4}
+    check_grid(grid ,borad_size)
 
 
 if __name__ == '__main__':
