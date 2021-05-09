ed83fa8bc842d7b934f30d7686cb4488e0883b1d
lirangazer <lirangazer@gmail.com>
2021-05-09 22:36:33 +0300

    working A* algorithm return the path in coordinate

--- venv/robot_A_star.py
+++ venv/robot_A_star.py
@@ -1,3 +1,4 @@
+import os
 from queue import PriorityQueue
 class Node:
     def __init__(self, parent = None, position = None):
@@ -6,9 +7,11 @@ class Node:
         self.g = 0 #distance to start
         self.h = 0
         self.f = 0
-    def __eq__(self, other):
-        return other.position == self.position
-
+    # def __eq__(self, other):
+    #     return other.position == self.position
+def GFS(grid ,start, end):
+    start_node = Node(None, start, end)
+    start_node.h=0
 
 def astar(grid, start, end ):
         start_node = Node(None, start)
@@ -32,25 +35,40 @@ def astar(grid, start, end ):
             open_list.pop(current_index)
             close_list.append(current_node)
 
-            if current_node == end_node:
+            if current_node.position == end_node.position:
                 path = []
                 current = current_node
                 while current is not None:
                     path.append(current.position)
+                    path.append(current.g)
                     current = current.parent
-                    return path[::-1]
+                return path[::-1]
             children = []
             #all_position = get_all_index(grid)
             (row,col) = current_node.position
-            neighbors = [(row-1,col), (row+1,col), (row,col-1), (row,col+1), (row+1,col+1), (row-1,col-1), (row-1,col+1), (row+1,col-1)]
+            neighbors = [(row-1,col), (row+1,col), (row,col-1), (row,col+1)]
             #get the next position if position out of board dont give add it to the next possible node
             for new_position in neighbors:
-                if (((new_position[0] or new_position[1]) < 0) or ((new_position[0] or new_position[1]) > len(grid[0]))):
-
+                counter = 0
+                for i in close_list:
+                    if new_position == i.position:
+                       counter+=1
+                if counter > 0 :
                     continue
+                if (((new_position[0]) or (new_position[1])) < 0):
+                    continue
+                elif ((new_position[0] > len(grid[0])-1) or (new_position[1] > len(grid[0])-1)):
+                    continue
+                elif new_position[1] < 0:
+                    continue
+                elif grid[new_position[0]][new_position[1]] == 'W':
+                    continue
+
                 else:
-                 node_position  = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+                 node_position  = (new_position[0], new_position[1])
+                 #node_position  = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
                  new_node=Node(current_node , node_position)
+                 print(new_node.position)
                  children.append(new_node)
             for child in children:
                 for close_child in close_list:
@@ -77,14 +95,6 @@ def astar(grid, start, end ):
                 open_list.append(child)
 
 
-
-
-                #print( grid[child.position[0]][child.position[1]])
-
-
-
-
-
 def check_grid(grid, board_size):
     numOfBolck = 0
     for i in grid:
@@ -98,8 +108,6 @@ def get_all_index(grid):
         for j in range(len(grid[i])):
             all_position.append((i,j))
     return (all_position)
-
-
 def foundStart(grid , board_size):
 
     #new and algant way
@@ -122,7 +130,6 @@ def foundStart(grid , board_size):
     if countFailure == board_size:
         print("there is no start position please check the input file")
     return
-
 def foundEnd(grid ,borad_size):
   # new and algant way
     # val = 'A'
@@ -141,27 +148,26 @@ def foundEnd(grid ,borad_size):
         print("there is no start position please check the input file")
     return
 
-
-
 def main():
-    input_file = open('files\input.txt','r')
-    lines = input_file.read().splitlines()
-    algorithem_type=lines[0]
-    borad_size = int(lines[1])
-    grid=[]
-    currentBoard = 0
-    count = 0
-    for line in lines[2:]:
-        grid.append(list(line))
+    filename = "C:\Liran_Collage\College\Fourth year\second simester\ML course\HM1\/files\input.txt"
+    with open(filename,'r') as input_file:
+        lines = input_file.read().splitlines()
+        algorithem_type=lines[0]
+        borad_size = int(lines[1])
+        grid=[]
+        currentBoard = 0
+        count = 0
+        for line in lines[2:]:
+         grid.append(list(line))
     print(grid)
     movement =['U','D','R','L']
-
     check_grid(grid ,borad_size)
     start = foundStart(grid,borad_size)
     end = foundEnd(grid ,borad_size)
     print("the start position : %s "  %(start,) )
     print("the end position : %s "  %(end,) )
-    astar(grid, start, end)
+    path = astar(grid, start, end)
+    print(path)
     #runRobot(start,grid,cost,movement)
 
 if __name__ == '__main__':
